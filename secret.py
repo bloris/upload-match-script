dcd726e6de61f50ba417c6c67b13f95e1a98dea6
Hoyoun Lee <hy2930@gmail.com>
2022-08-16 11:01:47 +0900

    change encoding

--- secret.py
+++ secret.py
@@ -1,26 +1,26 @@
 import json
 
 def api_key():
-    with open("secret.json") as f:
+    with open("secret.json", encoding = "UTF8") as f:
         json_p = json.loads(f.read())
     return json_p["api_key"]
 
 def firebase_key():
-    with open("secret.json") as f:
+    with open("secret.json", encoding = "UTF8") as f:
         json_p = json.loads(f.read())
     return json_p["firebase"],json_p["project_name"]
 
 def get_changed_name():
-    with open("secret.json") as f:
+    with open("secret.json", encoding = "UTF8") as f:
         json_p = json.loads(f.read())
     return json_p["changed_name"]
 
 def getDBKey():
-    with open("secret.json") as f:
+    with open("secret.json", encoding = "UTF8") as f:
         json_p = json.loads(f.read())
     return json_p["dbKey"]
 
 def getPath():
-    with open("secret.json") as f:
+    with open("secret.json", encoding = "UTF8") as f:
         json_p = json.loads(f.read())
     return json_p["history_path"]
